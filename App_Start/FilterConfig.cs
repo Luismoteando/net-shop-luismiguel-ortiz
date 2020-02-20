4625149279ee3773e28decdf0f25138ce0bcc4f9
LUIS MIGUEL ORTIZ ROZALÉN <luismiguel.ortiz1@alu.uclm.es>
2020-02-20 16:16:58 +0100

    Agregar archivos de proyecto.

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace net_shop_luismiguel_ortiz
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="net_shop_luismiguel_ortiz.MvcApplication" Language="C#" %>
